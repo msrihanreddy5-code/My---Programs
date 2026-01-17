f001431fcff0a62ab0116af054b8b5394c933d42
M.Srihan Reddy <msrihanreddy5@gmail.com>
2026-01-17 17:33:16 +0530

    P&C

---
+++ Permutations.cpp
@@ -0,0 +1,20 @@
+#include<simplecpp>
+
+long long factorial(int x){
+    long long f=1;
+    for(int i=1;i <=x;i++){
+        f *=i;
+    }
+    return f;
+}
+
+int main(){
+    int n,r; 
+    cin >> n >>r;
+
+    long long p = factorial(n) / factorial(n-r);
+    long long c = factorial(n) / (factorial(r) * factorial(n-r));
+
+    cout << p << " " << c;
+    return 0;
+}
